26e77e1ddd6c255deef64bdefedb5bb1fe748b14
arunstop <arunstop@gmail.com>
2020-07-23 20:41:13 +0700

    symptom show question fix

--- application/models/SymptomModel.php
+++ application/models/SymptomModel.php
@@ -14,14 +14,14 @@ class SymptomModel   extends CI_Model
         ORDER BY mg.ID_GEJALA
         ";
 
-        // $this->db->select('tgd.ID_GEJALA_DETAIL, tgd.ID_GEJALA, tgd.ID_PENYAKIT, mg.NAMA_GEJALA,mg.PERTANYAAN, mp.NAMA_PENYAKIT, tgd.YES, tgd.NO');
-        // $this->db->from('tr_gejala_detail as tgd');
-        // $this->db->join('ms_gejala as mg', 'tgd.ID_GEJALA = mg.ID_GEJALA');
-        // $this->db->join('ms_penyakit as mp', 'tgd.ID_PENYAKIT= mp.ID_PENYAKIT');
-        // $this->db->where(array_filter($dataArr, 'strlen'));
-        // $this->db->order_by('mg.ID_GEJALA');
+        $this->db->select('tgd.ID_GEJALA_DETAIL, tgd.ID_GEJALA, tgd.ID_PENYAKIT, mg.NAMA_GEJALA,mg.PERTANYAAN, mp.NAMA_PENYAKIT, tgd.YES, tgd.NO');
+        $this->db->from('tr_gejala_detail as tgd');
+        $this->db->join('ms_gejala as mg', 'tgd.ID_GEJALA = mg.ID_GEJALA');
+        $this->db->join('ms_penyakit as mp', 'tgd.ID_PENYAKIT= mp.ID_PENYAKIT');
+        $this->db->where(array_filter($dataArr, 'strlen'));
+        $this->db->order_by('mg.ID_GEJALA');
         
-        $query = $this->db->get_where('ms_gejala', $dataArr);
+        $query = $this->db->get();
         return $query->result_array();
     }
 
